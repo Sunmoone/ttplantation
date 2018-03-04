b217920049ea47a1853c06daec2b216c775c71eb
taojunlong <782570386@qq.com>
2018-03-04 16:04:23 +0800

    update
    Change-Id: Iefecd84086ba2ba8fbe6379f2b00e5a85d3ddd53

--- pages/my-tree/index.js
+++ pages/my-tree/index.js
@@ -5,9 +5,21 @@ const app = getApp()
 Page({
   data: {
     trees: [{
-      img: '',
+      img:'http://s15.sinaimg.cn/middle/611c7110hbe439d23d3de&690',
       name: '成仙核桃',
-      
+      province: '甘肃省',
+      city: '成县',
+      days: 17
     }]
+  },
+  onLoad: function () {
+    wx.setNavigationBarTitle({
+      title: '我的果树'
+    })
+  },
+  onItemTap: function(){
+    wx.navigateTo({
+      url: '../my-tree/detail',
+    })
   }
 })
