230276a2cb23b6627c1a0718e625d0187abe73c8
taojunlong <782570386@qq.com>
2018-03-08 00:15:39 +0800

    update
    Change-Id: I9d2721623f559bdc8eac0f7578a1bdd23a137922

--- pages/photo/index.js
+++ pages/photo/index.js
@@ -7,27 +7,38 @@ Page({
       src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
       date: '2018.2.3'
     }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
+        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+        date: '2018.2.4'
+      }, {
+        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+        date: '2018.2.4'
     }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
+      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+      date: '2018.2.4'
+      }, {
+        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+        date: '2018.2.4'
     }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
+      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+      date: '2018.2.4'
+      }, {
+        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+        date: '2018.2.4'
     }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
+      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+      date: '2018.2.4'
+      }, {
+        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+        date: '2018.2.4'
     }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
+      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+      date: '2018.2.4'
+      }, {
+        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+        date: '2018.2.4'
     }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
-    }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
-    }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
-    }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
-    }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
-    }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690'
+      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+      date: '2018.2.4'
     }]
   },
   onLoad: function () {
