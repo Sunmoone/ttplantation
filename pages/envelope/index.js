a05b2b744614daa680ec3d8f26b65b76c1b1811c
taojunlong <782570386@qq.com>
2018-03-09 23:49:01 +0800

    update
    Change-Id: I113c5fdd16c616df0874dc918dbda498041baa12

--- pages/envelope/index.js
+++ pages/envelope/index.js
@@ -7,13 +7,16 @@ Page({
   data: {
     envelopes: [{
       content: '请查看苹果长势',
-      from: '我的农场'
+      from: '我的农场',
+      date: '2018.06.23'
     },{
       content: '大魅甘肃欢迎你',
-      from: 'xx县政府'
+      from: 'xx县政府',
+      date: '2018.06.23'
     },{
       content: '村里人都开刹车',
-      from: '众泰汽车'
+      from: '众泰汽车',
+      date: '2018.06.23'
     }]
   },
   onItemTap: function(){
--- pages/photo/index.js
+++ pages/photo/index.js
@@ -4,46 +4,29 @@ const app = getApp()
 Page({
   data: {
     photos: [{
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
+      src: './sprout.png',
       date: '2018.2.3'
     }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-        date: '2018.2.4'
+      src: './sprout.png',
+      date: '2018.2.3'
       }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-        date: '2018.2.4'
+        src: './sprout.png',
+        date: '2018.2.3'
     }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-      date: '2018.2.4'
+      src: './sprout.png',
+      date: '2018.2.3'
       }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-        date: '2018.2.4'
+        src: './sprout.png',
+        date: '2018.2.3'
     }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-      date: '2018.2.4'
+      src: './sprout.png',
+      date: '2018.2.3'
       }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-        date: '2018.2.4'
+        src: './sprout.png',
+        date: '2018.2.3'
     }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-      date: '2018.2.4'
-      }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-        date: '2018.2.4'
-    }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-      date: '2018.2.4'
-      }, {
-        src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-        date: '2018.2.4'
-    }, {
-      src: 'http://s7.sinaimg.cn/middle/611c7110hbe439ea30d76&690',
-      date: '2018.2.4'
+      src: './sprout.png',
+      date: '2018.2.3'
     }]
-  },
-  onLoad: function () {
-    wx.setNavigationBarTitle({
-      title: '相册'
-    })
   }
 })
