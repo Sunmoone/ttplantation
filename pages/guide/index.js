888edebe07c85b9fb62d0163a7b0b61954198519
taojunlong <782570386@qq.com>
2018-03-18 02:04:25 +0800

    update
    Change-Id: I448d751341696fafac3316e9e8cedd0969ab99a2

--- pages/guide/index.js
+++ pages/guide/index.js
@@ -6,31 +6,14 @@ const app = getApp();
 Page({
     data: {},
     onLoad: function () {
-        //get planting_rid
-        const userInfo = app.globalData.userInfo;
-        var getDataPromise = new Promise((resolve, reject) => { //请求数据
-            getOrSave(userInfo, (res) => {
-                app.globalData.userInfo = res.data;
-                resolve(res);
-            });
-        });
-        var waitTimePromise = new Promise((resolve, reject) => { //计时器
-            setTimeout(() => {resolve('timer')}, 1500);
+      setTimeout(function () {
+        const user = app.globalData.userInfo
+        getOrSave(user, res => {
+          const rid = res.data.planting_rid
+          wx.redirectTo({
+            url: rid === -1 ? '../index/index' : ('../home/index?rid=' + rid),
+          })
         })
-        Promise.all([getDataPromise, waitTimePromise]).then((res) => {
-            if (res != 'timer') { // 过滤计时器的resolve
-                if (res.planting_rid == -1 || typeof(res.planting_rid) == "undefined") { // 没有种植记录
-                    wx.redirectTo({
-                        url: '../index/index'
-                    });
-                } else { // 如果有种植记录
-                    wx.redirectTo({
-                        url: `../home/index?rid=${res.planting_rid}`
-                    });
-                }
-            }
-        }).catch((e) => {
-            wx.showToast({ icon: 'none', title: '网络异常，启动失败' });
-        });
+      }, 1500)
     }
 })
