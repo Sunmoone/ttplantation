da66f77180abda9c753dd74024d45e9d43fbcaf1
wangyu <wangyu.fe@bytedance.com>
2018-03-26 22:41:09 +0800

    修改了获取个人信息的位置

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ const uid = parseInt(Math.random()*10000)
 App({
   onLaunch: function () {
     const that = this
-    wx.login({
-      success: function(res){
-        wx.getUserInfo({
-          success: function (res) {
-            that.globalData.userInfo = res.userInfo?res.userInfo:res.data.userInfo
-          }
-        })
-      },
-      fail: function (res) {
-        log(res)
-      }
-    })
+    // wx.login({
+    //   success: function(res){
+    //     log(res)
+    //     wx.getUserInfo({
+    //       success: function (res) {
+    //         log(res)            
+    //         that.globalData.userInfo = res.userInfo?res.userInfo:res.data.userInfo
+    //         log(that.globalData.userInfo)
+    //       }
+    //     })
+    //   },
+    //   fail: function (res) {
+    //     log(res)
+    //   }
+    // })
     const audio = wx.createInnerAudioContext()
     if(audio){
       audio.src = 'http://10.8.127.110/images/bg.mp3'
--- pages/guide/index.js
+++ pages/guide/index.js
@@ -1,4 +1,5 @@
 import { getOrSave } from '../../services/UserService.js'
+import { log } from '../../utils/util.js'
 
 // 引导页；
 const app = getApp();
@@ -6,20 +7,34 @@ const app = getApp();
 Page({
     data: {},
     onLoad: function () {
-      const { userId, user_id, nickName, name, avatarUrl, avatar_url } = app.globalData.userInfo
-      setTimeout(function(){
-        getOrSave({
-          user_id: userId || user_id,
-          name: nickName || name || userId || user_id,
-          avatar_url: avatarUrl || avatar_url,
-          screen_name: nickName || name || userId || user_id,
-          showLoading: false
-        }, res => {
-          app.globalData.userInfo = res.data
-          const rid = res.data.planting_rid
-          wx.redirectTo({
-            url: rid === -1 ? '../index/index' : ('../home/index?rid=' + rid),
-          })
+      setTimeout(function () {
+        wx.login({
+          success: function (res) {
+            wx.getUserInfo({
+              success: function (res) {
+                app.globalData.userInfo = res.userInfo ? res.userInfo : res.data.userInfo
+                const { 
+                  userId, user_id, nickName, name, avatarUrl, avatar_url 
+                } = app.globalData.userInfo
+                getOrSave({
+                  user_id: userId || user_id,
+                  name: nickName || name || userId || user_id,
+                  avatar_url: avatarUrl || avatar_url,
+                  screen_name: nickName || name || userId || user_id,
+                  showLoading: false
+                }, res => {
+                  app.globalData.userInfo = res.data
+                  const rid = res.data.planting_rid
+                  wx.redirectTo({
+                    url: rid === -1 ? '../index/index' : ('../home/index?rid=' + rid),
+                  })
+                })
+              }
+            })
+          },
+          fail: function (res) {
+            log(res)
+          }
         })
       }, 1500)
     }
